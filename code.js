67552e9dcfc1be044581e6d3dc50d428f94393cd
AverageCC <164907221+AverageCC@users.noreply.github.com>
2024-03-28 12:05:04 -0700

    Game update: Rock, Paper, Scissors

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function playGame() {
-    alert("This is a test to ensure that the code is being executed.");
+    var coices = ["rock", "paper", "scissors"];
+    var choice = prompt("Choose rock, paper, or scissors");
+
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        var computerChoice = choices[Math.floor(Math.random() * 3)];
+        alert("The computer chose " + computerChoice);
+
+        if (choice === computerChoice) {
+            alert("The result is a tie!");
+        } else if (
+            (choice === "rock" && computerChoice === "scissors") ||
+            (choice === "paper" && computerChoice === "rock") ||
+            (choice === "scissors" && computerChoice === "paper")
+        ) {
+            alert("You win!");
+        } else {
+            alert("You lose!");
+        }
+    }
 }
