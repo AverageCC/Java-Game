30404b4d08de122cff905333907aa4b0f1f9c7a0
AverageCC <164907221+AverageCC@users.noreply.github.com>
2024-04-02 15:02:12 -0700

    Completed: Web Game - v1.0

--- code.js
+++ code.js
@@ -5,13 +5,17 @@ function playGame() { // This function is called when the user clicks the button
 
     // This while loop checks if the user's response is valid. If the user's choice is not valid then the user is prompted to enter a valid choice.
     while (choices.indexOf(userChoice) === -1) {
-        userChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors.").toLowerCase();
+        if (userChoice === 'shoot') {
+            alert("You shouldn't bring a gun to a rock, paper, scissors fight!"); // This is my little easter egg. If the user types 'shoot' then this alert is displayed.
+            return; // This resets the user menu.
+        }
+        userChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors.").toLowerCase(); // This prompts the user to enter a valid choice with no regard of case sensitivity.
     }
 
     if(userChoice === computerChoice) { // This checks if the user and computer responses are the same. If they're the same then the It's a tie! alert is displayed.
         alert("It's a tie!");
     } else if( // This checks if the user has a winning combination.
-        (userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'scissors' && computerChoice === 'paper') || (userChoice === 'paper' && computerChoice === 'rock')
+        (userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'scissors' && computerChoice === 'paper') || (userChoice === 'paper' && computerChoice === 'rock') // || is used for OR
     ) {
         alert(`You win! ${userChoice} beats ${computerChoice}.`); // If the user wins then the alert displays the winning message.
     } else {
