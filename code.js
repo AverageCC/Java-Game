a5d328822189fc25ee392e26a37d2be215c94972
AverageCC <164907221+AverageCC@users.noreply.github.com>
2024-04-02 14:42:50 -0700

    Completed: Web Game - v1.0

--- code.js
+++ code.js
@@ -1,16 +1,15 @@
-function playGame() { // This function will run when the user clicks the button
-    var choices = ['rock', 'paper', 'scissors']; // This is a set of choices for rock paper scissors
-    var computerChoice = choices[Math.floor(Math.random() * 3)]; // This is the computer's choice
+function playGame() { // This function is called when the user clicks the button
+    const choices = ['rock', 'paper', 'scissors']; // const is a variable that cannot be reassigned which establishes the choices for the computer.
+    const computerChoice = choices[Math.floor(Math.random() * 3)]; // This line of code randomly selects a choice for the computer by generating a random number between 0 and 2 and then using that number to select a random option from the array.
+    const userChoice = prompt("Please choose and option. Rock, Paper or Scissors?").toLowerCase(); // This prompts the user to enter a choice without case sensitivity.
 
-    var userChoice = prompt("Do you choose rock, paper or scissors?").toLowerCase(); // This creates a prompt for the user to inout their choice. toLowerCase() makes it so that the input is not case sensitive
-
-    if(userChoice === computerChoice) { // This says that if the user choice and the computer choice are the same, then it's a tie
-        alert("It's a tie!"); // This will alert the user that it's a tie
-    } else if(
-        (userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'scissors' && computerChoice === 'paper') || (userChoice === 'paper' && computerChoice === 'rock') // These are all the winning combinations that trigger the, "You Won!" line
+    if(userChoice === computerChoice) { // This checks if the user and computer responses are the same. If they're the same then the It's a tie! alert is displayed.
+        alert("It's a tie!");
+    } else if( // This checks if the user has a winning combination.
+        (userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'scissors' && computerChoice === 'paper') || (userChoice === 'paper' && computerChoice === 'rock')
     ) {
-        alert("You win! " + userChoice + " beats " + computerChoice + "."); // This will alert the user that they won
+        alert(`You win! ${userChoice} beats ${computerChoice}.`); // If the user wins then the alert displays the winning message.
     } else {
-        alert("You lose! " + computerChoice + " beats " + userChoice + "."); // This will alert the user that they lost  only if none of the other conditions are met
+        alert(`You lose! ${computerChoice} beats ${userChoice}.`); // If the user loses then the alert displays the losing message.
     }
 }
