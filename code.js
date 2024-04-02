fc0b3d2f5d49c93911958dcec69aa7a690ee2f8b
AverageCC <164907221+AverageCC@users.noreply.github.com>
2024-04-02 14:50:44 -0700

    Completed: Web Game - v1.0

--- code.js
+++ code.js
@@ -3,6 +3,11 @@ function playGame() { // This function is called when the user clicks the button
     const computerChoice = choices[Math.floor(Math.random() * 3)]; // This line of code randomly selects a choice for the computer by generating a random number between 0 and 2 and then using that number to select a random option from the array.
     const userChoice = prompt("Please choose and option. Rock, Paper or Scissors?").toLowerCase(); // This prompts the user to enter a choice without case sensitivity.
 
+    // This while loop checks if the user's response is valid. If the user's choice is not valid then the user is prompted to enter a valid choice.
+    while (choices.indexOf(userChoice) === -1) {
+        userChoice = prompt("Invalid choice. Please choose Rock, Paper or Scissors.").toLowerCase();
+    }
+
     if(userChoice === computerChoice) { // This checks if the user and computer responses are the same. If they're the same then the It's a tie! alert is displayed.
         alert("It's a tie!");
     } else if( // This checks if the user has a winning combination.
